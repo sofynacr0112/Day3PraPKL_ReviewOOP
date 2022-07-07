72eac4cfe6e7a0b59103dadfcd14aa33731aef2c
sofynacr0112 <sofynacintarahmawati01@gmail.com>
2022-07-07 14:13:23 +0700

    sofyna

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php
+    class Database {
+        public $host = "localhost", $user = "root", $pass = "", $db = "db1";
+        public $koneksi;
+
+        public function __construct()
+        {
+            $this->koneksi = mysqli_connect(
+                $this->host,
+                $this->user,
+                $this->pass,
+                $this->db,
+            );
+
+            if ($this->koneksi) {
+                // echo "Berhasil";
+            }
+            else
+            {
+                echo "Koneksi Database Gagal";
+            }
+        }
+    }
+
+    $db = new Database();
+    
+    include 'guru.php';
+    // include 'mahasiswa.php';
+?>
